9241aab1e89bfd192593fa0b749b0986a9c4a6b5
Milan Banjac <tap3ah@tutanota.com>
2023-10-15 11:34:58 +0200

    pokretanje servisa & firewall

--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 dnf -y update
 dnf -y install mariadb
+dnf install -y mariadb-server
 dnf -y install httpd
 dnf -y install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 update-crypto-policies --set DEFAULT:SHA1
 dnf -y install nodejs
 update-crypto-policies --set DEFAULT
 dnf -y update
+systemctl enable mariadb.service
+systemctl enable httpd.service
+firewall-cmd --add-port=3000/tcp
+firewall-cmd --add-port=80/tcp
 # --skip-broken --nobest
