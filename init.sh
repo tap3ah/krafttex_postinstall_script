d45c09745ff970b97f9d569a9f8660dc0c28e448
Milan Banjac <tap3ahchina@google.com>
2023-10-19 14:13:20 +0200

    nodejs v20 -> v21

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@ dnf -y update
 dnf -y install mariadb
 dnf install -y mariadb-server
 dnf -y install httpd
-dnf -y install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+dnf install https://rpm.nodesource.com/pub_21.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 update-crypto-policies --set DEFAULT:SHA1
-dnf -y install nodejs
+dnf install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 update-crypto-policies --set DEFAULT
 dnf -y update
 systemctl enable mariadb.service
